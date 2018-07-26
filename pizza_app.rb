da9cb1018d2db4eef40e3e39930439cd2c54a0cc
JBrow95 <browningjustin95@gmail.com>
2018-07-26 11:41:57 -0400

    created nav bar

--- pizza_app.rb
+++ pizza_app.rb
@@ -1,36 +1,21 @@
-
-def rpizza()
-	puts "Here is a random pizza!"
-	puts "#{crust}#{meats}#{meats2}#{veggies}#{fruits}"
-end
-
 def crust()
 	crust = ['thin', 'pan']
-	puts "Crust: #{crust.sample}"
 end
 
 def meats()
 	m1 = ['none', 'bacon', 'ham', 'pepperoni', 'sausage']
-	puts "Meat Toppings: #{m1.sample}"
 end
 
 def meats2()
 	m2 = ['none', 'bacon', 'ham', 'pepperoni', 'sausage']
-	puts "2nd Meat Toppings: #{m2.sample}"
 end
 
 def veggies()
 	vegs = ['tomatoe', 'onion', 'peppers', "none"]
-	puts "Vegetables Toppings: #{vegs.sample}"
 end
 
-def fruits()
-	f = ["pineapple", "none"]
-	puts "Fruit Toppigss: #{f.sample}"
-end
 
-	puts "how many pizzas do you want?"
-	response = gets.chomp.to_i
+	respons
 
 p_price = 0.0
 while response > 0 do 
